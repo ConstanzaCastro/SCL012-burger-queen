f1ed4ea765da8deb33e35dbe4fa86da2ba54b16f
ConstanzaCastro <redlight.down@gmail.com>
2020-02-25 16:19:17 -0300

    vinculación con firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/database';
+
+/*const firebaseConfig = {
+    apiKey: "AIzaSyDMXPev4HLlLT8O314ABR33NqjqgIdr5BQ",
+    authDomain: "vurgerkuin.firebaseapp.com",
+    databaseURL: "https://vurgerkuin.firebaseio.com",
+    projectId: "vurgerkuin",
+    storageBucket: "vurgerkuin.appspot.com",
+    messagingSenderId: "641952554912",
+    appId: "1:641952554912:web:2ccffd0d20445b8ed38ec5",
+    measurementId: "G-65GX98F9T6"
+  };
