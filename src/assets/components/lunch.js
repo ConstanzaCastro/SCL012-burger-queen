88135a2cd6f8b7b090417d25422feeb86db548f0
ConstanzaCastro <redlight.down@gmail.com>
2020-03-16 11:08:27 -0300

    Componente lunch

--- src/assets/components/lunch.js
+++ src/assets/components/lunch.js
@@ -1,3 +1,15 @@
-/*import React, { Component } from 'react'
-import db from '../firebase';*/
+import React, { Component } from 'react'
+import db from '../firebase';
+
+class Lunch extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            products: products.Lunch,
+            client: "",
+            table: "",
+            orders: [],
+            price: 0
+        }
+    }
 
