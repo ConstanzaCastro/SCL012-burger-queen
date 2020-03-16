1a46b8dac6cf02a3acb381dbea77dc3db75e5ee3
ConstanzaCastro <redlight.down@gmail.com>
2020-03-16 12:17:33 -0300

    lunch

--- src/assets/components/lunch.js
+++ src/assets/components/lunch.js
@@ -5,11 +5,27 @@ class Lunch extends Component{
     constructor(props){
         super(props)
         this.state={
-            products: products.Lunch,
+            products: data.Lunch,
             client: "",
             table: "",
             orders: [],
             price: 0
         }
     }
+
+    clickItem=(e)=>{ 
+
+        let products = this.state.orders;
+        products.push({
+          product: e.product,
+          price: e.price
+        });
+
+        let currentTotal = this.state.price;
+        let sumTotal = e.price;
+        let newTotal = currentTotal + sumTotal;
+        this.setState({
+          price: newTotal
+        });
+       }
 
